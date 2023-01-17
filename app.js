c9088c50b4962e2865d7f6219a3f8b3fe47aba28
Sebastian Hård Samils <sebastian.samils@gmail.com>
2023-01-17 21:22:43 +0100

    DB connection established

--- app.js
+++ app.js
@@ -3,19 +3,42 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
+
 const postsAPI = require("./api/posts");
+const authAPI = require("./api/auth");
+const passport = require("passport");
+const session = require("express-session");
+require("./config/passport");
+
+require("dotenv").config();
 
 const app = express();
+
+// Set up mongoose connection
+const mongoose = require("mongoose");
+// eslint-disable-next-line no-undef
+const mongoDB = process.env.DB;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
 
 app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+// app.use(passportConfig);
+
+// eslint-disable-next-line no-undef
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(passport.authenticate("session"));
 // eslint-disable-next-line no-undef
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/v1/posts", postsAPI);
+app.use("/api/v1/auth", authAPI);
 app.use("/", (req, res) => res.sendStatus(400));
 
 module.exports = app;
