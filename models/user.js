1d3ae7fa0dabfa6b0efaa849a56242252b20e819
Sebastian Hård Samils <sebastian.samils@gmail.com>
2023-01-17 21:22:54 +0100

    user model added

---
+++ models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  first_name: { type: String, min: 1, max: 100, required: true },
+  last_name: { type: String, min: 1, max: 100, required: true },
+  email: { type: String, min: 1, max: 100, required: true },
+  password: { type: String, min: 1, max: 100, required: true },
+  admin: { type: Boolean, default: false },
+  avatar: { type: String },
+  posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+});
+
+UserSchema.virtual("url").get(function () {
+  return `/user/${this._id}`;
+});
+
+UserSchema.virtual("full_name").get(function () {
+  return `${this.first_name}, ${this.last_name}`;
+});
+
+module.exports = mongoose.model("User", UserSchema);
