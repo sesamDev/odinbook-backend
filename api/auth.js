cd3de90bd04a15f736c9d199fe39d6a7584a11f9
Sebastian Hård Samils <sebastian.samils@gmail.com>
2023-01-19 13:06:44 +0100

    register endpoint added

--- api/auth.js
+++ api/auth.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const authController = require("../controllers/authController");
 
 router.post("/login", authController.login_with_email);
+router.post("/register", authController.register_with_email);
 
 module.exports = router;
