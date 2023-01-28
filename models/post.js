bf1bb37f491c69ebf8d4484b71e9ecf30963dd45
Sebastian Hård Samils <sebastian.samils@gmail.com>
2023-01-28 17:00:14 +0100

    Added likes and removed title from schema

--- models/post.js
+++ models/post.js
@@ -3,11 +3,10 @@ const Schema = mongoose.Schema;
 const { DateTime } = require("luxon");
 
 const PostSchema = new Schema({
-  title: { type: String, min: 1, max: 100, required: true },
   text: { type: String, min: 1, max: 100, required: true },
-  likes: { type: Number, default: 0 },
   author: { type: Schema.Types.ObjectId, ref: "User", required: true },
   timestamp: { type: Date, default: Date.now() },
+  likes: { type: Number, default: 0 },
 });
 
 PostSchema.virtual("timestamp_formatted").get(function () {
