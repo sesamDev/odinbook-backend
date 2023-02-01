b005188a32959e18e7b554a9013fa7295f4fa2ab
Sebastian Hård Samils <sebastian.samils@gmail.com>
2023-02-01 20:27:30 +0100

    Added random names

--- seed.js
+++ seed.js
@@ -2,14 +2,70 @@ const { faker } = require("@faker-js/faker/locale/en");
 const User = require("./models/user");
 require("dotenv").config();
 
-function createRandomUser(n) {
-  return {
-    first_name: "Fake",
-    last_name: "User " + n,
-    email: faker.internet.exampleEmail(),
+const names = [
+  "Ervin Darnell",
+  "Ali Hadley",
+  "Tanner Duong",
+  "Alexzander Rucker",
+  "Carson Bruce",
+  "Tayla Willson",
+  "Elias Whiting",
+  "Tracy Zapata",
+  "Montana Lennon",
+  "Bayleigh Quinones",
+  "Ximena Luu",
+  "Jaycie Connor",
+  "Armando Hatcher",
+  "Santana Lackey",
+  "Natali Sizemore",
+  "Clark Small",
+  "Haden Malley",
+  "Juana Bagwell",
+  "Elliott Bui",
+  "Daquan Chapin",
+  "Serena Barlow",
+  "Esperanza Dickerson",
+  "Jana Akers",
+  "Deontae Christian",
+  "Braeden Neeley",
+  "Aysia Packer",
+  "Maximillian Fletcher",
+  "Eduardo Mark",
+  "Gissel Perry",
+  "Brannon Irvine",
+  "Citlalli Bolton",
+  "Tyrique Sinclair",
+  "Ryan Tavares",
+  "Johan Burnett",
+  "Tasha Mohr",
+  "Joanne Frantz",
+  "Israel Zielinski",
+  "Juliana Almeida",
+  "Bryanna Lindsay",
+  "Jordin Payton",
+  "Krista Womack",
+  "Iyanna Byers",
+  "Elisha Dye",
+  "Dana Amaro",
+  "Savion Knox",
+  "Lyle Dockery",
+  "Zion Barnhill",
+  "Donna Vazquez",
+  "Cailyn Westbrook",
+  "Savion Libby",
+];
+function splitName(name) {
+  return name.split(" ");
+}
+
+function createRandomUser(firstName, lastName) {
+  return new User({
+    first_name: firstName,
+    last_name: lastName,
+    email: firstName + lastName + "@odinbook.com",
     password: faker.internet.password(),
     avatar: faker.image.avatar(),
-  };
+  });
 }
 
 // Set up mongoose connection
@@ -28,13 +84,51 @@ db.on("error", console.error.bind(console, "connection error:"));
 // // //   avatar: { type: String, default: defaultAvatar },
 // // //   posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
 
-Array.from({ length: 1 }).forEach((v, idx) => {
-  const user = new User(createRandomUser(idx + 1));
-  user.save((err) => {
-    if (err) return console.log("Failed to save user to DB");
-    return console.log(user + " : Saved to DB!");
-  });
+// ADD USERS TO DB ----->
+
+// let users = [];
+// names.forEach((name) => {
+//   let firstName = splitName(name)[0];
+//   let lastName = splitName(name)[1];
+
+//   let user = createRandomUser(firstName, lastName);
+//   users.push(user);
+//   user.save((err) => {
+//     if (err) return console.log("Failed to save user to DB");
+//     return console.log(user + " : Saved to DB!");
+//   });
+// });
+// console.table(users);
+
+//<------- END
+
+// users.forEach((user) => {
+//   let u = user
+//   user.save((err) => {
+//     if (err) return console.log("Failed to save user to DB");
+//     return console.log(user + " : Saved to DB!");
+//   });
+// });
+
+User.find({ _id: { $ne: "63c98aef48230828f1a7b439" } }).exec((err, users) => {
+  if (err) return console.log(err);
+
+  User.findById("63c98aef48230828f1a7b439")
+    .update({ friends: users })
+    .exec((err) => {
+      if (err) return console.log("Error updating user", err);
+      console.log("Friends added");
+    });
 });
+
+// console.table(usersArr);
+
+// User.findOne({ email: "testuser@odinbook" })
+//   .update({ friends: "63cbdc803834e383152ef84f" })
+//   .exec((err, user) => {
+//     if (err) console.log("ERROR: ", err);
+//     console.log(user);
+//   });
 
 db.off;
 
